02b6284925728c1fb43307d2825a4c0169ed49a0
chrisESGI <barreto.christophe@gmail.com>
2013-11-14 16:10:16 +0100

    Ajout fichier javascript

---
+++ Content/js/action.js
@@ -0,0 +1,11 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Chris
+ * Date: 14/11/13
+ * Time: 16:01
+ * To change this template use File | Settings | File Templates.
+ */
+function writeMail()
+{
+    document.getElementById("loginInput").value = document.getElementById("lnameInput").value + "." +document.getElementById("fnameInput").value;
+}
