cccaffb5a85bd4ab2eda47968e2a21d73f5153c1
titiyo <bondeau.yohann@gmail.com>
2014-01-30 23:46:24 +0100

    correct error

--- ServiceWeb/server.php
+++ ServiceWeb/server.php
@@ -1,7 +1,7 @@
 <?php
 	require_once '../Model/ModelUser.php';
 	require_once '../Framework/Controller.php';
-	require_once '../Framework/Model.php';
+	require_once '../Framework/ModelWebService.php';
 
 	ini_set("soap.wsdl_cache_enabled","0");
 	$server = new SoapServer("server.wsdl");
